3f661b6ddfde6bd3b7c134fca1ea3f3d43efd5d3
Tomoya Yoshioka <tomoya.y@icloud.com>
2020-10-20 21:00:56 +0000

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { Handler } from "aws-lambda"
 import { myLog } from "./lib/myLog"
 
-export function main() {
-  myLog("hello, world")
+export const handler: Handler<any, any> = (event: any, result: any) => {
+  myLog(`event: ${event}`)
+  myLog(`result: ${result}`)
 }
